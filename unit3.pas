edcd6570427c80b918c31e5da8348c03726e2e51
MarsSerg <mydadwasmars@yandex.ru>
2017-05-24 21:48:10 +0300

    Wtf is going on

--- unit3.pas
+++ unit3.pas
@@ -5,93 +5,99 @@ unit Unit3;
 interface
 
 uses
-                  Classes, SysUtils, sqldb, db, mssqlconn, FileUtil, Forms,
-		  Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,Unit4;
+Classes, SysUtils, sqldb, DB, mssqlconn, FileUtil, Forms,
+Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, Unit4;
 
 type
 
-		  { TForm3 }
+{ TForm3 }
 
-                  TForm3 = class(TForm)
-				    Brdname2: TLabel;
-				    Brdname3: TLabel;
-				    DataSource1: TDataSource;
-				    GroupBox1: TGroupBox;
-				    grpskate1: TGroupBox;
-				    grpskate2: TGroupBox;
-				    grpbox: TGroupBox;
-				    Image1: TImage;
-				    Brdname1: TLabel;
-				    Image2: TImage;
-				    Image3: TImage;
-				    lbldskr1: TLabel;
-				    lbldskr2: TLabel;
-				    lbldskr3: TLabel;
-				    Lblabout: TLabel;
-				    Lblabout1: TLabel;
-				    Lblabout2: TLabel;
-				    MSSQLConnection1: TMSSQLConnection;
-				    PnlNext: TPanel;
-				    Qryinfo: TSQLQuery;
-				    SQLTransaction1: TSQLTransaction;
-				    procedure FormActivate(Sender: TObject);
-				    procedure FormCreate(Sender: TObject);
-				    procedure Formopen(Sender: TObject);
-                                    Procedure LoadInfo;
-				    procedure PnlNextClick(Sender: TObject);
+TForm3 = class(TForm)
+  Brdname2: TLabel;
+  Brdname3: TLabel;
+  DataSource1: TDataSource;
+  GroupBox1: TGroupBox;
+  grpskate1: TGroupBox;
+  grpskate2: TGroupBox;
+  grpbox: TGroupBox;
+  Image1: TImage;
+  Brdname1: TLabel;
+  Image2: TImage;
+  Image3: TImage;
+  lbldskr1: TLabel;
+  lbldskr2: TLabel;
+  lbldskr3: TLabel;
+  Lblabout: TLabel;
+  Lblabout1: TLabel;
+  Lblabout2: TLabel;
+  MSSQLConnection1: TMSSQLConnection;
+  PnlNext: TPanel;
+  Qryinfo: TSQLQuery;
+  SQLTransaction1: TSQLTransaction;
+  procedure FormActivate(Sender: TObject);
+  procedure FormCreate(Sender: TObject);
+  procedure Formopen(Sender: TObject);
+  procedure FormShow(Sender: TObject);
+  procedure PnlNextClick(Sender: TObject);
 
-                  private
-                                    { private declarations }
-                  public
-                                    { public declarations }
-                                    function GetIsNullValue(q1:TSQLQuery;fieldname:string):string;
-                  end;
+
+private
+  { private declarations }
+public
+  { public declarations }
+  function GetIsNullValue(q1: TSQLQuery;
+    fieldname: string): string;
+
+  procedure LoadInfo;
+end;
 
 var
-                  Form3: TForm3;
-                  recstatus:integer;
+Form3: TForm3;
+recstatus: integer;
 
 implementation
 
 {$R *.lfm}
-function TForm3.GetIsNullValue(q1:TSQLQuery;fieldname:string):string;
+function TForm3.GetIsNullValue(q1: TSQLQuery; fieldname: string): string;
 begin
 
 end;
 
-Procedure Tform3.LoadInfo;
+procedure Tform3.LoadInfo;
 
 begin
 
-          case recstatus of
-             1: begin
-             lbldskr1.Caption:=qryinfo.FieldByName('Description').asstring;
-             Brdname1.Caption:=qryinfo.FieldByName('Skate_Name').asstring;
-             end;
-             2:begin
-               lbldskr2.Caption:=qryinfo.FieldByName('Description').asstring;
-                Brdname2.Caption:=qryinfo.FieldByName('Skate_Name').asstring;
-	     end;
-	     3:begin
-                 lbldskr3.Caption:=qryinfo.FieldByName('description').asstring;
-                 Brdname3.Caption:=qryinfo.FieldByName('Skate_Name').asstring;
-	     end;
-	  end;
-
+  case recstatus of
+    1 :
+      begin
+      lbldskr1.Caption := qryinfo.FieldByName('Description').AsString;
+      Brdname1.Caption := qryinfo.FieldByName('Skate_Name').AsString;
+      end;
+    2 :
+      begin
+      lbldskr2.Caption := qryinfo.FieldByName('Description').AsString;
+      Brdname2.Caption := qryinfo.FieldByName('Skate_Name').AsString;
+      end;
+    3 :
+      begin
+      lbldskr3.Caption := qryinfo.FieldByName('description').AsString;
+      Brdname3.Caption := qryinfo.FieldByName('Skate_Name').AsString;
+      end;
+    end;
 
 end;
 
 
 procedure TForm3.PnlNextClick(Sender: TObject);
 begin
-
-
-          for recstatus:=1 to 3 do begin
-            qryinfo.Next;
-            if (not qryinfo.eof) then
-               loadinfo;
-	end;
+  if (not qryinfo.EOF) then
+     for recstatus := 1 to 3 do
+     begin
+          qryinfo.Next;
+          if (not qryinfo.EOF) then
+          loadinfo;
      end;
+end;
 
 
 
@@ -102,32 +108,34 @@ end;
 
 procedure TForm3.FormCreate(Sender: TObject);
 begin
-     recstatus:=0;
-     if (recstatus=0) then begin
-        qryinfo.Active:=false;
-        //qryinfo.SQL.Clear;
-        //qryinfo.SQL.Add('Exec dbo.Skates');
- //       qryinfo.Active:=true;
- //       inc(recstatus);
- //       loadinfo;
- //
- //       for recstatus:=2 to 3 do begin
- //           qryinfo.Next;
- //           if (not qryinfo.eof) then
- //              loadinfo;
-	//end;
+  recstatus := 0;
+  if (recstatus = 0) then
+    begin
+    qryinfo.Active := False;
+    //qryinfo.SQL.Clear;
+    //qryinfo.SQL.Add('Exec dbo.Skates');
+    //       qryinfo.Active:=true;
+    //       inc(recstatus);
+    //       loadinfo;
 
-     end;
+    //       for recstatus:=2 to 3 do begin
+    //           qryinfo.Next;
+    //           if (not qryinfo.eof) then
+    //              loadinfo;
+    //end;
+
+    end;
 end;
 
 procedure TForm3.Formopen(Sender: TObject);
 begin
-      form4.showmodal;
+  form4.showmodal;
 end;
 
-
-
-
+procedure TForm3.FormShow(Sender: TObject);
+begin
+     //PnlNextClick(pnlnext);
+end;
 
 
 
