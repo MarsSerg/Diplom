00da09ce4feb1b0ce781ce8abb870a581948d70c
MarsSerg <mydadwasmars@yandex.ru>
2017-06-06 21:06:57 +0300

    Almost ready

--- backup/unit2.pas.bak
+++ backup/unit2.pas.bak
@@ -5,8 +5,8 @@ unit Unit2;
 interface
 
 uses
-Classes, SysUtils, sqldb, db, FileUtil, Forms, Controls, Graphics,
-Dialogs, StdCtrls, ExtCtrls, DbCtrls;
+Classes, SysUtils, sqldb, DB, FileUtil, Forms, Controls, Graphics,
+Dialogs, StdCtrls, ExtCtrls, DBCtrls;
 
 type
 
@@ -16,31 +16,40 @@ TForm2 = class(TForm)
   Bearings: TLabel;
   Boardstyle: TLabel;
   dsBoardStyle: TDataSource;
+  dsTracks: TDataSource;
+  dsWheels: TDataSource;
+  dsBear: TDataSource;
+  Lblpath: TLabel;
   lcmbBoardStyle: TDBLookupComboBox;
   dsAdminSkate: TDataSource;
   Fordescr: TEdit;
   Lblname: TLabel;
   chkRidingStyles: TCheckGroup;
   Forname: TEdit;
-  forbrdstl: TComboBox;
-  forbrng: TComboBox;
-  fortracks: TComboBox;
-  forwheels: TComboBox;
   GroupBox2: TGroupBox;
   Image1: TImage;
   Imgload: TPanel;
   Lblname1: TLabel;
+  lcmbTracks: TDBLookupComboBox;
+  lcmbWheels: TDBLookupComboBox;
+  lcmbBearings: TDBLookupComboBox;
   //OpenDialog1: TOpenDialog;
   OpenDialog2: TOpenDialog;
   Panel1: TPanel;
+  qryaddrdstl: TSQLQuery;
+  qryTracks: TSQLQuery;
+  qryWheels: TSQLQuery;
+  qryBear: TSQLQuery;
   Ridingstyle: TLabel;
   qryAdminSkate: TSQLQuery;
   qryBoardStyle: TSQLQuery;
+  qryadd: TSQLQuery;
   Tracks: TLabel;
   Wheels: TLabel;
   procedure FormCreate(Sender: TObject);
   procedure FormShow(Sender: TObject);
   procedure ImgloadClick(Sender: TObject);
+  procedure Panel1Click(Sender: TObject);
 private
   { private declarations }
 public
@@ -49,7 +58,9 @@ end;
 
 var
 Form2: TForm2;
-strRidingStyles:TStringList;
+strRidingStyles: TStringList;
+tr1:TSQLTransaction;
+
 implementation
 
 {$R *.lfm}
@@ -61,6 +72,7 @@ begin
   if OpenDialog2.Execute then
       try
       Image1.Picture.LoadFromFile(OpenDialog2.FileName);
+      lblpath.Caption:=OpenDialog2.FileName;
       except
       on EInvalidGraphic do
         ShowMessage('Ошибка загрузки')
@@ -69,39 +81,72 @@ begin
       end;
 end;
 
+procedure TForm2.Panel1Click(Sender: TObject);
+var
+name_txt ,Descr_txt,Tr_id ,brd_id,wh_id,bear_id,path:string;
+begin
+   name_txt:= forname.Text;
+   Descr_txt:= Fordescr.Text;
+   tr_id:=lcmbTracks.KeyValue;
+   bear_id:=lcmbBearings.KeyValue;
+   wh_id:=lcmbWheels.KeyValue;
+   brd_id:=lcmbBoardStyle.KeyValue;
+   path:=Lblpath.Caption;
+   qryadd.SQL.Text:='exec dbo.AddSkate @Picture=:path, @Boardstyle_ID=:brd_id, @Tracks_ID=:tr_id, @Wheels_ID=:wh_id, @Bearings_ID=:bear_id, @Name=:name_txt, @Description=:Descr_txt';
+   qryadd.ParamByName('path').AsString := path;
+   qryadd.ParamByName('brd_id').AsInteger := StrToInt(brd_id) ;
+   qryadd.ParamByName('tr_id').AsInteger := StrToInt(tr_id);
+   qryadd.ParamByName('wh_id').AsInteger := StrToInt(wh_id);
+   qryadd.ParamByName('bear_id').AsInteger := StrToInt(bear_id);
+   qryadd.ParamByName('name_txt').AsString := name_txt;
+   qryadd.ParamByName('descr_txt').AsString := Descr_txt;
+   qryadd.ExecSQL;
+   tr1.Commit;
+   for i:=1 to chkRidingStyles.items.Count do begin
+       if (chkRidingStyles.Checked[i]) then
+           begin
+
+	   end;
+   end;
+end;
+
 procedure TForm2.FormShow(Sender: TObject);
 var
-   txtSql:string;
-   txtElement:string;
+  txtSql: string;
+  txtElement: string;
 
 begin
-   qryAdminSkate.Active:=false;
+  qryAdminSkate.Active := False;
 
-   //загрузка Riding_style
-   txtSql:='select Ridingstyle_id,ridingstyle from Riding_Style';
-   qryAdminSkate.SQL.Text:=txtSql;
-   qryAdminSkate.Active:=true;
+  //загрузка Riding_style
+  txtSql := 'select Ridingstyle_id,ridingstyle from Riding_Style';
+  qryAdminSkate.SQL.Text := txtSql;
+  qryAdminSkate.Active := True;
 
 
-   strRidingStyles.Clear;
-   chkRidingStyles.Items.Clear;
-   while (not qryAdminSkate.EOF) do begin
-         txtElement:=qryAdminSkate.FieldByName('Ridingstyle_id').AsString;
-         strRidingStyles.Add(txtElement);
-         txtElement:=qryAdminSkate.FieldByName('Ridingstyle').AsString;
-         chkRidingStyles.Items.Add(txtElement);
-         qryAdminSkate.Next;
-   end;
-   qryAdminSkate.Active:=false;
+  strRidingStyles.Clear;
+  chkRidingStyles.Items.Clear;
+  while (not qryAdminSkate.EOF) do
+    begin
+    txtElement := qryAdminSkate.FieldByName('Ridingstyle_id').AsString;
+    strRidingStyles.Add(txtElement);
+    txtElement := qryAdminSkate.FieldByName('Ridingstyle').AsString;
+    chkRidingStyles.Items.Add(txtElement);
+    qryAdminSkate.Next;
+    end;
+  qryAdminSkate.Active := False;
 
-   qryBoardStyle.Active:=true;
-   lcmbBoardStyle.Refresh ;
+  qryBoardStyle.Active := True;
+  qryWheels.Active := True;
+  qryBear.Active := True;
+  qryTracks.Active := True;
+  //lcmbBoardStyle.Refresh ;
 
 end;
 
 procedure TForm2.FormCreate(Sender: TObject);
 begin
-     strRidingStyles:=TStringList.Create;
+  strRidingStyles := TStringList.Create;
 end;
 
 
--- unit3.pas
+++ unit3.pas
@@ -42,7 +42,7 @@ TForm3 = class(TForm)
 
   procedure FormCreate(Sender: TObject);
   procedure FormShow(Sender: TObject);
-  procedure Formopen(curlabel:Tlabel);
+  procedure Formopen(curlabel: Tlabel);
   procedure PnlNextClick(Sender: TObject);
   procedure LoadPicture(img: TImage);
 
@@ -59,7 +59,7 @@ var
 Form3: TForm3;
 recstatus: integer;
 mass_skate_id: array [1..3] of integer;
-roll_direction:boolean;
+roll_direction: boolean;
 
 implementation
 
@@ -92,7 +92,7 @@ begin
       Brdname1.Caption := qryinfo.FieldByName('Skate_Name').AsString;
       LoadPicture(Image1);
       mass_skate_id[counter] := qryinfo.FieldByName('Skate_id').AsInteger;
-      Lblabout.Tag:=mass_skate_id[counter];
+      Lblabout.Tag := mass_skate_id[counter];
       Inc(counter);
       end;
     2 :
@@ -101,7 +101,7 @@ begin
       Brdname2.Caption := qryinfo.FieldByName('Skate_Name').AsString;
       LoadPicture(Image2);
       mass_skate_id[counter] := qryinfo.FieldByName('Skate_id').AsInteger;
-      Lblabout1.Tag:=mass_skate_id[counter];
+      Lblabout1.Tag := mass_skate_id[counter];
       Inc(counter);
       end;
     3 :
@@ -110,7 +110,7 @@ begin
       Brdname3.Caption := qryinfo.FieldByName('Skate_Name').AsString;
       LoadPicture(Image3);
       mass_skate_id[counter] := qryinfo.FieldByName('Skate_id').AsInteger;
-      Lblabout2.Tag:=mass_skate_id[counter];
+      Lblabout2.Tag := mass_skate_id[counter];
       end;
     end;
 
@@ -122,22 +122,22 @@ begin
   if (not qryinfo.Active) then
     qryinfo.Active := True;
   if (not qryinfo.EOF) then
-      for recstatus:=1 to 3 do
+    for recstatus := 1 to 3 do
+      begin
+      qryinfo.Next;
+      if (qryinfo.EOF) then
         begin
-        qryinfo.Next;
-        if (qryinfo.EOF) then
-          begin
-          qryinfo.First;
-          end;
-        loadinfo;
+        qryinfo.First;
         end;
+      loadinfo;
+      end;
 
 end;
 
 procedure TForm3.FormCreate(Sender: TObject);
 begin
   recstatus := 0;
-  roll_direction:=true;
+  roll_direction := True;
   if (recstatus = 0) then
     begin
     qryinfo.Active := False;
@@ -156,56 +156,65 @@ begin
     end;
 end;
 
-procedure TForm3.Formopen(curlabel:Tlabel);
+procedure TForm3.Formopen(curlabel: Tlabel);
 var
-  lbl,i:integer;
-  img:string;
-  str:string;
-  s1:string;
+  lbl, i: integer;
+  img: string;
+  str: string;
+  s1: string;
 begin
-      lbl:=curlabel.Tag;
-      qryonepage.Active := False;
-      qryonepage.SQL.Clear;
-      qryonepage.SQL.Add('Exec dbo.Skates @idskate=:lbl');
-      qryonepage.ParamByName('lbl').AsInteger := lbl;
-      qryonepage.Active:=true;
-      Form4.Name_of_deck.Caption:= qryonepage.FieldByName('Skate_name').AsString;
-      img:=qryonepage.FieldByName('Picture').AsString;;
-      Form4.Image1.Picture.LoadFromFile(img);
-      Form4.LblDescription.Caption:=qryonepage.FieldByName('Description').AsString;
-      str:=Form4.LblDescription.Caption;
-      s1:='';
-        for i:=1 to length(str) do begin
-        s1:=s1+str[i];
-        if Form4.LblDescription.canvas.TextWidth(s1)>=Form4.Grpdskr.Width then begin
-                      Form4.LblDescription.Caption:=Form4.LblDescription.Caption+s1+' ';
-                      s1:='';
-                      end;
-        end;
-       Form4.Lblstyles.Caption:=qryonepage.FieldByName('Styles').AsString;
-       Form4.Lblnose.Caption:=Form4.Lblnose.Caption+'      -     '+qryonepage.FieldByName('Nose').AsString;
-       Form4.Lbltail.Caption:=Form4.Lbltail.Caption+'      -     '+qryonepage.FieldByName('Tale').AsString;
-       Form4.Lblwidth.Caption:='      -     '+qryonepage.FieldByName('Width').AsString;
-       Form4.Lbllength.Caption:='      -     '+qryonepage.FieldByName('Length').AsString;
-       Form4.Lbldcktp.Caption:=Form4.Lbldcktp.Caption+'      -     '+qryonepage.FieldByName('Type_of_deck').AsString;
-       Form4.Lbltrsize.Caption:='  -     '+qryonepage.FieldByName('Tracks_Size').AsString;
-       Form4.Lbltrtype.Caption:=Form4.Lbltrtype.Caption+'      -     '+qryonepage.FieldByName('Tracks_Type').AsString;
-       Form4.Lblwhhrd.Caption:='   -     '+qryonepage.FieldByName('Wh_Hardness').AsString;
-       Form4.Lblwhsize.Caption:='  -     '+qryonepage.FieldByName('Wheel_Size').AsString;
-       Form4.Lblbrname.Caption:=Form4.Lblbrname.Caption+'      -     '+qryonepage.FieldByName('Bear_name').AsString;
-       Form4.Lblmdof.Caption:=Form4.Lblmdof.Caption+'      -     '+qryonepage.FieldByName('Madeof').AsString;
-       Form4.Lblwhlbs.Caption:='      -     '+qryonepage.FieldByName('Wheelbase').AsString;
+  lbl := curlabel.Tag;
+  qryonepage.Active := False;
+  qryonepage.SQL.Clear;
+  qryonepage.SQL.Add('Exec dbo.Skates @idskate=:lbl');
+  qryonepage.ParamByName('lbl').AsInteger := lbl;
+  qryonepage.Active := True;
+  Form4.Name_of_deck.Caption := qryonepage.FieldByName('Skate_name').AsString;
+  img := qryonepage.FieldByName('Picture').AsString;
+  ;
+  Form4.Image1.Picture.LoadFromFile(img);
+  Form4.LblDescription.Caption := qryonepage.FieldByName('Description').AsString;
+  str := Form4.LblDescription.Caption;
+  s1 := '';
+  for i := 1 to length(str) do
+    begin
+    s1 := s1 + str[i];
+    if Form4.LblDescription.canvas.TextWidth(s1) >= Form4.Grpdskr.Width then
+      begin
+      Form4.LblDescription.Caption := Form4.LblDescription.Caption + s1 + ' ';
+      s1 := '';
+      end;
+    end;
+  Form4.Lblstyles.Caption := qryonepage.FieldByName('Styles').AsString;
+  Form4.Lblnose.Caption :=
+    Form4.Lblnose.Caption + '      -     ' + qryonepage.FieldByName('Nose').AsString;
+  Form4.Lbltail.Caption :=
+    Form4.Lbltail.Caption + '      -     ' + qryonepage.FieldByName('Tale').AsString;
+  Form4.Lblwidth.Caption := '      -     ' + qryonepage.FieldByName('Width').AsString;
+  Form4.Lbllength.Caption := '      -     ' + qryonepage.FieldByName('Length').AsString;
+  Form4.Lbldcktp.Caption :=
+    Form4.Lbldcktp.Caption + '      -     ' + qryonepage.FieldByName('Type_of_deck').AsString;
+  Form4.Lbltrsize.Caption :=
+    '  -     ' + qryonepage.FieldByName('Tracks_Size').AsString;
+  Form4.Lbltrtype.Caption :=
+    Form4.Lbltrtype.Caption + '      -     ' + qryonepage.FieldByName('Tracks_Type').AsString;
+  Form4.Lblwhhrd.Caption :=
+    '   -     ' + qryonepage.FieldByName('Wh_Hardness').AsString;
+  Form4.Lblwhsize.Caption := '  -     ' + qryonepage.FieldByName('Wheel_Size').AsString;
+  Form4.Lblbrname.Caption :=
+    Form4.Lblbrname.Caption + '      -     ' + qryonepage.FieldByName('Bear_name').AsString;
+  Form4.Lblmdof.Caption :=
+    Form4.Lblmdof.Caption + '      -     ' + qryonepage.FieldByName('Madeof').AsString;
+  Form4.Lblwhlbs.Caption :=
+    '      -     ' + qryonepage.FieldByName('Wheelbase').AsString;
 
-       form4.showmodal;
+  form4.showmodal;
 end;
 
 procedure TForm3.FormShow(Sender: TObject);
 begin
   PnlNextClick(pnlnext);
 end;
-
-
-
 
 
 
